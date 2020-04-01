63604a2fc35137992ec9c558f7e4f4eba0284ef5
Dan <jhairguz.t@gmail.com>
2020-03-31 22:27:34 -0500

    feat: service & controller for person implemented

--- beginner-boot/src/main/java/com/guzman/beginnerboot/api/PersonController.java
+++ beginner-boot/src/main/java/com/guzman/beginnerboot/api/PersonController.java
@@ -2,15 +2,20 @@ package com.guzman.beginnerboot.api;
 
 import com.guzman.beginnerboot.model.Person;
 import com.guzman.beginnerboot.service.PersonService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PersonController {
 
     private final PersonService personService;
+
 
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
+    @PostMapping
     public void addPerson(Person person) {
         personService.addPerson(person);
     }
--- beginner-boot/src/main/java/com/guzman/beginnerboot/service/PersonService.java
+++ beginner-boot/src/main/java/com/guzman/beginnerboot/service/PersonService.java
@@ -3,6 +3,7 @@ package com.guzman.beginnerboot.service;
 import com.guzman.beginnerboot.dao.PersonDao;
 import com.guzman.beginnerboot.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
